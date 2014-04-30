130a6d5115dd8a3b55aacef9be432d11d94da37a
Owen Trueblood <jmptable@mit.edu>
2014-04-30 00:41:25 -0400

    Random picture and progress tracking.

--- util/labeller/app.js
+++ util/labeller/app.js
@@ -1,5 +1,22 @@
 var express = require("express");
 var app = express();
+var fs = require("fs");
+
+var corpusData = {};
+var imageNames = fs.readdirSync("./public/corpus");
+
+function progress() {
+    var total = imageNames.length;
+    var completed = 0;
+    
+    for(var i in imageNames) {
+        if(imageNames[i] in corpusData) {
+            completed++;
+        }
+    }
+
+    return 100*completed/total;
+}
 
 // static server
 app.use(express.static(__dirname + '/public'));
@@ -13,9 +30,19 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
-app.post('/submit/:image', function (req, res){
+app.get('/random', function (req, res) {
+    var name = imageNames[Math.floor(Math.random()*imageNames.length)]
+    res.send({name: name});
+});
+
+app.get('/progress', function (req, res) {
+    res.send({progress: progress()});
+});
+
+app.post('/submit/:image', function (req, res) {
     var image = req.params.image;
-    console.log(image, req.body);
+    corpusData[image] = req.body;
+    console.log(JSON.stringify(corpusData));
 });
 
 var port = Number(process.env.PORT || 5000);
