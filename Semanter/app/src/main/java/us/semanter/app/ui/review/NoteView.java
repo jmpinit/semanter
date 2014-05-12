e6cd1e8e3c1487e46252e4896c780ceb6dcde1c0
Owen Trueblood <jmptable@mit.edu>
2014-05-12 00:29:19 -0400

    Zooming and improved panning.

--- Semanter/app/src/main/java/us/semanter/app/ui/review/NoteView.java
+++ Semanter/app/src/main/java/us/semanter/app/ui/review/NoteView.java
@@ -20,9 +20,9 @@ public class NoteView extends VisionView {
     private Note note;
     private Bitmap noteImage;
 
-    private float lastZoom, zoom;
+    private float startZoom, currentZoom, imageZoom;
     private PointF startPosition, currentPosition, imagePosition;
-    private boolean inMotion;
+    private boolean panning, zooming;
 
     public NoteView(Context ctx, AttributeSet attrSet) {
         super(ctx, attrSet);
@@ -33,12 +33,16 @@ public class NoteView extends VisionView {
     }
 
     public void setNote(Note note) {
-        zoom = 1.0f;
-        lastZoom = 1.0f;
+        startZoom = 1.0f;
+        currentZoom = 1.0f;
+        imageZoom = 1.0f;
+
         currentPosition = new PointF(0, 0);
         startPosition = new PointF(0, 0);
         imagePosition = new PointF(0, 0);
-        inMotion = false;
+
+        panning = false;
+        zooming = false;
 
         this.note = note;
 
@@ -53,50 +57,104 @@ public class NoteView extends VisionView {
         canvas.drawColor(Color.BLACK);
 
         if(noteImage != null) {
-            if(inMotion) {
-                float dx = currentPosition.x - startPosition.x;
-                float dy = currentPosition.y - startPosition.y;
-                canvas.translate(imagePosition.x + dx, imagePosition.y + dy);
+            float z;
+            if(zooming) {
+                z = imageZoom + (currentZoom - startZoom);
             } else {
-                canvas.translate(imagePosition.x, imagePosition.y);
+                z = imageZoom;
             }
 
-            float z = lastZoom + zoom;
-            //canvas.scale(z, z);
+            canvas.scale(z, z, getWidth()/2.0f, getHeight()/2.0f);
+
+            if(panning) {
+                float dx = currentPosition.x - startPosition.x;
+                float dy = currentPosition.y - startPosition.y;
+
+                canvas.translate((imagePosition.x + dx)/z, (imagePosition.y + dy)/z);
+            } else {
+                canvas.translate(imagePosition.x/z, imagePosition.y/z);
+            }
+
             canvas.drawBitmap(noteImage, 0, 0, p);
         }
     }
-    
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch(event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                if(event.getPointerCount() == 1) {
-                    currentPosition.set(event.getX(), event.getY());
-                    startPosition.set(event.getX(), event.getY());
+        switch(event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                // panning
+                currentPosition.set(event.getX(), event.getY());
+                startPosition.set(event.getX(), event.getY());
 
-                    inMotion = true;
+                panning = true;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                panning = false;
+
+                // zooming
+                float x1 = event.getX(0);
+                float y1 = event.getY(0);
+                float x2 = event.getX(1);
+                float y2 = event.getY(1);
+
+                float distance = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                currentZoom = distance / (Math.max(getWidth(), getHeight()) / ZOOM_FACTOR);
+                startZoom = currentZoom;
+
+                zooming = true;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                if (event.getPointerCount() == 1) {
+                    if(panning)
+                        currentPosition.set(event.getX(), event.getY());
+                } else if (event.getPointerCount() == 2) {
+                    if(zooming) {
+                        float x1 = event.getX(0);
+                        float y1 = event.getY(0);
+                        float x2 = event.getX(1);
+                        float y2 = event.getY(1);
+
+                        float distance = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                        currentZoom = distance / (Math.max(getWidth(), getHeight()) / ZOOM_FACTOR);
+                    }
                 }
                 break;
+            }
 
-            case MotionEvent.ACTION_MOVE:
-                if(event.getPointerCount() == 1) {
-                    currentPosition.set(event.getX(), event.getY());
-                }
-                break;
-
-            case MotionEvent.ACTION_UP:
-                if(event.getPointerCount() == 1) {
+            case MotionEvent.ACTION_UP: {
+                if(panning) {
                     currentPosition.set(event.getX(), event.getY());
 
+                    // panning
                     float dx = currentPosition.x - startPosition.x;
                     float dy = currentPosition.y - startPosition.y;
 
                     imagePosition.set(imagePosition.x + dx, imagePosition.y + dy);
 
-                    inMotion = false;
+                    panning = false;
                 }
+
                 break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP: {
+                if(zooming) {
+                    // zooming
+                    float deltaZoom = currentZoom - startZoom;
+                    imageZoom += deltaZoom;
+
+                    zooming = false;
+                }
+
+                break;
+            }
         }
 
         return true;
